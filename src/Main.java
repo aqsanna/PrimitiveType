e2f4e348e7b054b15fc04868454dda8ac59b0529
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-09-07 22:57:03 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b1 = 127;
+        short b2 = 32726;
+        int b3 = 2147483647;
+        long b4 = 925544255800000000L;
+        float f;
+        f = 12.5F;
+        double l;
+        l = 148.5;
+        char ch1 = 'Y';
+        char ch2 = 'M';
+        char ch3 = 88;
+        boolean b;
+        b = false;
+        boolean t = true;
+      /**
+       *
+       *
+       * ToDo esdd
+       */
+        System.out.println("b1 is: " + b1);
+        System.out.println("b2 is: " + b2);
+        System.out.println("b3 is: " + b3);
+        System.out.println("b4 is: " + b4);
+        System.out.println("f is: " + f);
+        System.out.println("ch1 is: " + ch1);
+        System.out.println("ch2 is: " + ch2);
+        System.out.println("ch3 is: " + ch3);
+        System.out.println("b is: " + b);
+        System.out.println("t is: " + t);
+
+    }
+}

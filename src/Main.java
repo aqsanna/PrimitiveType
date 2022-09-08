1b7cfc29e3651f8a5c8650e8456800cb179703dc
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-09-08 22:30:56 +0400

    Lesson1 homework

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,72 @@
 public class Main {
     public static void main(String[] args) {
+
+        // byte type
+        byte a1 = 0;
         byte b1 = 127;
-        short b2 = 32726;
+        byte m1 = -128;
+        System.out.println("byte type example");
+        System.out.println("a1 is: " + a1);
+        System.out.println("b1 is: " + b1);
+        System.out.println("m1 is: " + m1);
+
+        // short type
+        short a2 = 129;
+        short b2 = 32767;
+        short m2 = -32768;
+        System.out.println("short type example");
+        System.out.println("a2 is: " + a2);
+        System.out.println("b2 is: " + b2);
+        System.out.println("m2 is: " + m2);
+
+        // int type
+        int a3 = 32768;
         int b3 = 2147483647;
-        long b4 = 925544255800000000L;
-        float f;
-        f = 12.5F;
-        double l;
-        l = 148.5;
+        int m3 = -2147483648;
+        System.out.println("int type example");
+        System.out.println("a3 is: " + a3);
+        System.out.println("b3 is: " + b3);
+        System.out.println("m3 is: " + m3);
+
+        // long type
+        long a4 = 2147483648L;
+        long b4 = 9223372036854775807L;
+        long m4 = -9223372036854775808L;
+        System.out.println("long type example");
+        System.out.println("a4 is: " + a4);
+        System.out.println("b4 is: " + b4);
+        System.out.println("b4 is: " + b4);
+
+        // float type
+        float f5;
+        f5 = 12.5F;
+        float m5 = -922000003372036800000054775808F;
+        System.out.println("float type example");
+        System.out.println("f5 is: " + f5);
+        System.out.println("m5 is: " + m5);
+
+        // double type
+        double l6;
+        l6 = 148.5;
+        double m6 = -92200000337203680000005477580.4551114;
+        System.out.println("double type example");
+        System.out.println("l6 is: " + l6);
+        System.out.println("m6 is: " + m6);
+
+        //char type
         char ch1 = 'Y';
         char ch2 = 'M';
         char ch3 = 88;
-        boolean b;
-        b = false;
-        boolean t = true;
-      /**
-       *
-       *
-       * ToDo esddtest
-       */
-        System.out.println("b1 is: " + b1);
-        System.out.println("b2 is: " + b2);
-        System.out.println("b3 is: " + b3);
-        System.out.println("b4 is: " + b4);
-        System.out.println("f is: " + f);
+        System.out.println("char type example");
         System.out.println("ch1 is: " + ch1);
         System.out.println("ch2 is: " + ch2);
         System.out.println("ch3 is: " + ch3);
+
+        //boolean type
+        boolean b;
+        b = false;
+        boolean t = true;
+        System.out.println("boolean type example");
         System.out.println("b is: " + b);
         System.out.println("t is: " + t);
 
